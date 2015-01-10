ed359bc1bd47715324baec31087cc583522ce372
Aman Tiwari <aman26@gmail.com>
2015-01-10 17:31:08 +0000

    Made happy_sad.py into a module

--- happy_sad.py
+++ happy_sad.py
@@ -1,62 +1,71 @@
 import nltk
-import numpy as np
 from nltk.corpus import wordnet as wn
 from itertools import chain
 
+
+#Example list
 goods = ['good', 'happiness', 'birth', 'life', 'justice', 'love', 'benefit'
          'sunshine', 'warmth', 'acceptance', 'smile', 'laugh', 'laughter', 'joy']
 sads = ['bad', 'sadness', 'kill', 'death', 'unjust', 'hate', 'loss', 'lose',
         'war', 'sorrow', 'atrocity', 'tears', 'hitler']
 
-ss_happy    = frozenset(chain(*[wn.synsets(good) for good in goods]))
-ss_sad      = frozenset(chain(*[wn.synsets(sad) for sad in sads]))
-#print ss_happy
 
-def slch(to_test, feel):
-    o_list = []
+def build_synsets(words):
+    """Builds a flat list of synsets from a list of words"""
+    return frozenset(chain(*[wn.synsets(words) for word in words]))
+
+def slch(to_test, targets):
+    """Takes a list of synsets to test and a list of target synsets & finds the lch similarity"""
+    similarities = []
     for i in to_test:
-            for j in feel:
+            for j in targets:
                     try:
-                            o_list.append(i.lch_similarity(j))
-                    except nltk.corpus.reader.wordnet.WordNetError:
+                            similarities.append(i.lch_similarity(j))
+                    except nltk.corpus.reader.wordnet.WordNetError: #Ignore incomparable types of words
                         pass
-    return o_list
+    return similarities
 
-def wup(to_test, feel):
-    o_list = []
+def wup(to_test, targets):
+    """ Takes a list of synsets to test and a list of target synsets & finds the wup similarity"""
+    similarities = []
     for i in to_test:
-            for j in feel:
+            for j in targets:
                     try:
-                            o_list.append(i.wup_similarity(j))
-                    except:
+                            similarities.append(i.wup_similarity(j))
+                    except nltk.corpus.reader.wordnet.WordNetError: #Ignore incomparable types
                         pass
-    return o_list
-while True:
-    to_test = wn.synsets(raw_input(' : '))
+    return similarities
 
-    happy = [s for s in slch(to_test, ss_happy) if s != None]
-    sad = [s for s in slch(to_test, ss_sad) if s != None]
-    happy.sort(reverse=True), sad.sort(reverse=True)
-    if not happy:
-        happy = [0]
-    if not sad:
-        sad = [0]
-    h_mean = float(sum(happy[:10]))/10
-    s_mean = float(sum(sad[:10]))/10
+if __name__ == '__main__':
+    ss_happy    = build_synsets(goods)
+    ss_sad      = build_synsets(sads)
+    #print ss_happy
+    while True:
+        to_test = wn.synsets(raw_input(' : '))
 
-    print 'Happy: ' + str(h_mean)
-    print 'Sad: ' + str(s_mean)
-    print 'Wup'
+        happy = [s for s in slch(to_test, ss_happy) if s != None]
+        sad = [s for s in slch(to_test, ss_sad) if s != None]
+        happy.sort(reverse=True), sad.sort(reverse=True)
+        if not happy:
+            happy = [0]
+        if not sad:
+            sad = [0]
+        h_mean = float(sum(happy[:10]))/10
+        s_mean = float(sum(sad[:10]))/10
 
-    happy = [s for s in wup(to_test, ss_happy) if s != None]
-    sad = [s for s in wup(to_test, ss_sad) if s != None]
-    happy.sort(reverse=True), sad.sort(reverse=True)
-    if not happy:
-        happy = [0]
-    if not sad:
-        sad = [0]
-    h_mean = float(sum(happy))/len(sad)
-    s_mean = float(sum(sad))/len(sad)
+        print 'Happy: ' + str(h_mean)
+        print 'Sad: ' + str(s_mean)
+        print 'Wup'
 
-    print 'Happy: ' + str(h_mean)
-    print 'Sad: ' + str(s_mean)
+        happy = [s for s in wup(to_test, ss_happy) if s != None]
+        sad = [s for s in wup(to_test, ss_sad) if s != None]
+        happy.sort(reverse=True), sad.sort(reverse=True)
+        if not happy:
+            happy = [0]
+        if not sad:
+            sad = [0]
+        h_mean = float(sum(happy))/len(sad)
+        s_mean = float(sum(sad))/len(sad)
+
+        print 'Happy: ' + str(h_mean)
+        print 'Sad: ' + str(s_mean)
